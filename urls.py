ee98c10dac8481f9d6561087e3a2283061fb6491
Garre-Chandana <147966249+Garre-Chandana@users.noreply.github.com>
2026-04-10 09:27:30 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [path("index.html", views.index, name="index"),
+               path("UserLogin.html", views.UserLogin, name="UserLogin"),	      
+               path("UserLoginAction", views.UserLoginAction, name="UserLoginAction"),
+               path("TrainML", views.TrainML, name="TrainML"),
+	       path("Predict.html", views.Predict, name="Predict"),
+               path("PredictAction", views.PredictAction, name="PredictAction"),	       
+]
